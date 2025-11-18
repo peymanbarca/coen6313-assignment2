d7234b78a216f761e11963bea0387b9f7be370b6
p.yazdanian <p.yazdanian74@gmail.com>
2025-11-17 19:51:24 -0500

    modified api gateway routing to all microservices

--- gateway/main.py
+++ gateway/main.py
@@ -4,10 +4,11 @@ from fastapi import FastAPI, Request, Response
 import httpx
 import asyncio
 
-app = FastAPI()
+app = FastAPI(title='API Gateway')
 
 USER_V1 = os.getenv("USER_V1_URL", "http://localhost:8001")
 USER_V2 = os.getenv("USER_V2_URL", "http://localhost:8002")
+ORDER = os.getenv("ORDER_URL", "http://localhost:8003")
 STRANGLER_PROB = float(os.getenv("STRANGLER_PROB", "0.7"))  # fraction to distribute routing to v1
 
 client = httpx.AsyncClient()
@@ -24,6 +25,22 @@ def pick_target_user_service():
 @app.api_route("/users/{path:path}", methods=["POST", "PUT", "GET"])
 async def proxy(request: Request, path: str = ""):
     target = pick_target_user_service()
+    url = f"{target}{request.url.path}"
+    if request.url.query:
+        url = url + "?" + request.url.query
+    headers = dict(request.headers)
+
+    # Remove host header to avoid mismatch
+    headers.pop("host", None)
+    body = await request.body()
+    resp = await client.request(request.method, url, headers=headers, content=body, timeout=30.0)
+    return Response(status_code=resp.status_code, content=resp.content, headers=resp.headers)
+
+
+@app.api_route("/orders", methods=["POST", "PUT", "GET"])
+@app.api_route("/orders/{path:path}", methods=["POST", "PUT", "GET"])
+async def order_proxy(request: Request, path: str = ""):
+    target = ORDER
     url = f"{target}{request.url.path}"
     if request.url.query:
         url = url + "?" + request.url.query
